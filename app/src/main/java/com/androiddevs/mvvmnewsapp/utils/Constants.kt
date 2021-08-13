0f671a8454f67f9a901fedf1b7aafbdf6fce7327
Nilay Yıldız <1nilay.yildiz@gmail.com>
2021-08-13 13:51:51 +0300

    Breaking News Response Handled

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "612e3b2e13374534b41d7c78731e9105"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
