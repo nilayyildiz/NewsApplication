e8fbfadef39724e4a01d4caf02eaee9b0c8c5687
Nilay Yıldız <1nilay.yildiz@gmail.com>
2021-08-13 14:28:27 +0300

    Bottom Navigation View bug fixed

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,9 @@ import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.ui.fragments.BreakingNewsFragment
+import com.androiddevs.mvvmnewsapp.ui.fragments.SavedNewsFragment
+import com.androiddevs.mvvmnewsapp.ui.fragments.SearchNewsFragment
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
@@ -22,10 +26,28 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        val breakingNewsFragment = BreakingNewsFragment()
+        val savedNewsFragment = SavedNewsFragment()
+        val searchNewsFragment = SearchNewsFragment()
+
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
-        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
+        bottomNavigationView.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.breakingNewsFragment -> setCurrentFragment(breakingNewsFragment)
+                R.id.savedNewsFragment -> setCurrentFragment(savedNewsFragment)
+                R.id.searchNewsFragment -> setCurrentFragment(searchNewsFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.breakingNewsFragment, fragment)
+            commit()
+        }
 
 }
